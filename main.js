729b6b3c0b140e3d8fd228214c79c8e01fcfe5b3
Andrew Lincoln <andrewjlincoln@gmail.com>
2018-10-09 16:14:04 -0700

    Adds search feature

--- main.js
+++ main.js
@@ -33,29 +33,85 @@ document.addEventListener('DOMContentLoaded', function() {
              ${newBoxObj.name}</span><br>
             <i>In this box:</i> ${newBoxObj.items.join(', ')}
             </p>
-            <button class='align-baseline delete-box btn btn-primary'>Delete this box</button>
+            <div class="btn-group pull-right" role="group" aria-label="...">
+              <button class='align-baseline btn btn-primary delete-box'><span class= 'glyphicon glyphicon-trash'></span> Delete </button>
+              <button class='align-baseline btn btn-primary'>Unpacked!</button>
+            </div>
             
             
             `
 
         document.getElementById('box-field').appendChild(newBox)
         document.getElementById('box-add').reset()
-        document.getElementById('box-name').focus()
+       // document.getElementById('box-name').scrollIntoView()
         
     })
-
+//Deleting boxes
     document.addEventListener('click', function(event) {
       if (event.target.matches('.delete-box')) {
           let boxerString = '' + event.target.parentNode.textContent;
           let boxInd = Number(boxerString.charAt(27))
           alert(`Great! Possessions unlocked: ${allBoxes[boxInd - 1].items}!`)
           console.log(allBoxes[boxInd - 1])
-         
-      
           event.target.parentNode.remove()
       }
 
     })
+// Search
+    let searchBtn = document.getElementById('search-btn')  
+    let alertBar = document.getElementById('alert-bar')
+    searchBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+      let searchTerm = document.getElementById('search-box').value
+      if (allBoxes.length === 0) {
+        alertBar.innerHTML = `You can't search, you haven't packed anything!<button id="search-null">Oh yeah lol</button>`
+        alertBar.classList.remove('alert-success')
+        alertBar.classList.add('alert-danger')
+        alertBar.classList.remove('hidden')
+        let okBtnBad = document.getElementById('search-null')
+        okBtnBad.addEventListener('click', function() {
+            alertBar.classList.add('hidden')
+            document.getElementById('search-box').value = ''
+        })
+           
+      }
+      let foundArray = [];
+      for (let box of allBoxes) {
+          for (let boxItem of box.items) {
+            if (boxItem.toUpperCase() === searchTerm.toUpperCase()) {
+                foundArray.push(`#${box.number} - ${box.name}`)      
+            }          
+           }//for
+       }//for
+       if (foundArray.length > 0) {
+            alertBar.innerHTML=`Success--${searchTerm} found in: ${foundArray.join(', ')}.  <button id='search-good'>Nice, thanks!</button>`
+            alertBar.classList.remove('alert-danger')
+            alertBar.classList.add('alert-success')
+            alertBar.classList.remove('hidden')
+
+            let okBtnGood = document.getElementById('search-good')
+            okBtnGood.addEventListener('click', function() {
+              alertBar.classList.add('hidden')
+              document.getElementById('search-box').value = ''
+            })
+       } else {
+                alertBar.innerHTML = `Sorry, you don't have one of those! <button id='search-bad'>Oh, bummer.</button>`
+                if (!searchTerm) { alertBar.innerHTML = `Enter an item first! <button id='search-bad'>right, right</button>` }
+                alertBar.classList.remove('alert-success')
+                alertBar.classList.add('alert-danger')
+                alertBar.classList.remove('hidden')
+                let okBtnBad = document.getElementById('search-bad')
+
+                okBtnBad.addEventListener('click', function() {
+                    alertBar.classList.add('hidden')
+                    document.getElementById('search-box').value = ''
+                })
+       }//else
+              
+   
+
+    }) //search btn event
+  
 
 
 
